f5386afcd32aba38f5ee3f35e24e1828bb60a391
Jon Krake <1024jon@gmail.com>
2022-06-20 14:43:56 -0600

    add ifs

--- command.py
+++ command.py
@@ -3,7 +3,6 @@ import argparse
 parser = argparse.ArgumentParser(description="Pi Signage Command Script",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument("-s", "--shutdown", action="store_true", help="shutdown")
 parser.add_argument("-r", "--restart", action="store_true", help="reboot")
 parser.add_argument("-c", "--content", action="store_true", help="re-download content")
 parser.add_argument("-p", "--screenshot",action="store_true", help="update screenshot")
@@ -12,5 +11,17 @@ parser.add_argument("--tvoff",action="store_true", help="turn tv off")
 args = parser.parse_args()
 config = vars(args)
 
-if config['shutdown']:
-    print("shutting down")
+if config['restart']:
+    print("restarting")
+
+if config['content']:
+    print("re-downloading content")
+
+if config['screenshot']:
+    print("re-uploading screenshot")
+
+if config['tvon']:
+    print("trying to turn tv on")
+
+if config['tvoff']:
+    print("trying to turn tv off")
