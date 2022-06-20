3b1742e0c45a053562d1264ec2d5d869c65f48d7
Jon Krake <1024jon@gmail.com>
2022-06-20 15:53:26 -0600

    make screenshot a function

--- command.py
+++ command.py
@@ -25,14 +25,7 @@ if config['content']:
     print("re-downloading content")
 
 if config['screenshot']:
-    with httpx.AsyncClient() as client:
-        piName = os.uname()[1]
-        os.environ['DISPLAY'] = ':0'
-        raspi2png = subprocess.run(["scrot", "-o", "-z", f"/tmp/{piName}.png"])
-        
-        data = {'piName': piName}
-        files = {'file': open(f'/tmp/{piName}.png', 'rb')}
-        r = client.post(f'{BASE_URL}/UploadPiScreenshot', data=data, files=files)
+    screenshot()
 
 if config['tvon']:
     echo = subprocess.Popen(('echo', 'on','0'), stdout=subprocess.PIPE)
@@ -48,3 +41,13 @@ if config['tvoff']:
     echo = subprocess.Popen(('echo', 'standby', '0'), stdout=subprocess.PIPE)
     cec = subprocess.check_output(('cec-client', '-s', '-d', '1'), stdin=echo.stdout)
     echo.wait()
+
+async def screenshot():
+    async with httpx.AsyncClient() as client:
+        piName = os.uname()[1]
+        os.environ['DISPLAY'] = ':0'
+        raspi2png = subprocess.run(["scrot", "-o", "-z", f"/tmp/{piName}.png"])
+        
+        data = {'piName': piName}
+        files = {'file': open(f'/tmp/{piName}.png', 'rb')}
+        r = await client.post(f'{BASE_URL}/UploadPiScreenshot', data=data, files=files)
