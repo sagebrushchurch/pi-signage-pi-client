3bdf2b4237868d0efb3cc080b8c51aedcbd13d85
Jon Krake <1024jon@gmail.com>
2023-06-30 22:50:45 -0600

    change to ffplay

--- pisignage.py
+++ pisignage.py
@@ -76,15 +76,16 @@ def kill(proc_pid):
 
 # Define various pids
 def avPID():
-    pid = subprocess.Popen(["cvlc",
-                            "--video-wallpaper",
-                            "--no-osd",
-                            "mouse-hide-timeout",
-                            "1",
-                            "-L",
-                            "/tmp/signageFile"],
+    pid = subprocess.Popen(["ffplay",
+                            "-i",
+                            "/tmp/signageFile",
+                            "-loop",
+                            "0",
+                            "-fs",
+                            "-fast"],
                             stdout=subprocess.DEVNULL,
                             stderr=subprocess.STDOUT)
+
     return pid
 
 def otherFilePID():
