f71db38ef83b5df12b812af8f759f3dcb2acef05
Jon Krake <1024jon@gmail.com>
2026-09-13 09:13:17 -0600

    fix version #

--- pisignage.py
+++ pisignage.py
@@ -24,7 +24,7 @@ if '-dev-' in PI_NAME.lower():
 else:
     BASE_URL = 'https://piman.sagebrush.work/pi_manager_api'
 
-PI_CLIENT_VERSION = '2.8.4b'
+PI_CLIENT_VERSION = '2.8.5'
 
 
 def get_device_model():
